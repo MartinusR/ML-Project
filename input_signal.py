247a448afb6fac4b425387be958f48f4aeaed471
MartinusR <martinruffel@free.fr>
2017-05-30 23:52:35 +0200

    Decoder, comments

--- input_signal.py
+++ input_signal.py
@@ -1,8 +1,6 @@
-
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
-
 from spike_network import SpikeNetwork
 
 
@@ -39,23 +37,25 @@ def simulation(I, S, sigma=2e3, delta_t=1e-4, eta=6e-3):
     return x, network
 
 
-# for the moment being, just call this funciton with network.o and delta_t
+# for the moment being, just call this function with network.o and delta_t
 # all of these plot functions will be put in a separate file later
 def plot_from_o(o, delta_t):
     evt = events_matrix(np.transpose(o), delta_t * np.arange(len(o)))
     plot_spike_trains(evt, (0, delta_t*len(o)))
     plt.show()
 
+
 # The result time is in s
 def events_matrix(M, T):
     res = []
     for row in M:
         events = []
-        for i,s in enumerate(row):
+        for i, s in enumerate(row):
             if s == 1:
                 events.append(T[i])
         res.append(events)
     return res
+
 
 def plot_spike_trains(trains, ts, ax=None, ofs=1.3, lw=0.25, xlab=True):
     if ax is None:
@@ -67,3 +67,12 @@ def plot_spike_trains(trains, ts, ax=None, ofs=1.3, lw=0.25, xlab=True):
     if xlab:
         ax.set_xlabel("time (s)")
     plt.tight_layout()
+
+
+"""
+Probleme :
+Si on ne prend pas une signal grand en entree, pas de spike en sortie (pour moins de 1000 !)
+Si on prend un signal grand, ca finit par devenir instable avec un neurone qui a un énorme poids Forward, une énorme inhibition
+mais qui est insuffisante. Ainsi, par périodes, il fire plein plein de fois.
+Mettre mu = 100 règle le probleme, mais bon ça n'est pas du tout satisfaisant !
+"""
